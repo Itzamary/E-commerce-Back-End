6a86cb10ff7b03bf0ba00c630be13362f45fde72
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-25 19:23:28 -0500

    added the get one route to the product-routes.js file.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
+// be sure to include its associated Category and Tag data
 // get all products
 router.get('/', (req, res) => {
   // find all products
@@ -27,13 +27,36 @@ router.get('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   })
-  // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'product_tags'
+      }
+    ]
+  })
+  .then(dbProductsData => res.json(dbProductsData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
